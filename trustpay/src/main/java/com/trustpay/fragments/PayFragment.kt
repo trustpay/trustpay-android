ead54b83bd37b5e28e29b8b6d8b601cc125609de
DESKTOP-N68PHJU\Habibi <gildastema3@gmail.com>
2019-06-08 23:08:04 +0100

    add recycler operator

--- trustpay/src/main/java/com/trustpay/fragments/PayFragment.kt
+++ trustpay/src/main/java/com/trustpay/fragments/PayFragment.kt
@@ -3,10 +3,13 @@ package com.trustpay.fragments
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.trustpay.R
+import com.trustpay.adapters.OperatorAdapter
 import kotlinx.android.synthetic.main.fragment_pay.view.*
 
 
@@ -29,6 +32,7 @@ class PayFragment : Fragment() {
         id = arguments?.getString("id").toString()
         view.tv_name.text = name
         view.tv_amount.text = amount
+        view.recycler_operator.adapter = OperatorAdapter()
         return view
     }
 
